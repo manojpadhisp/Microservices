02246cf905e5b8521600a8967f64f7d7d7a19f14
Manoj <manojpadhisp@gmail.com>
2023-09-13 17:51:04 +0530

    EUREKA SERVER WITH ALL SERVICE COMMUNICATION

--- UserService/UserService/src/main/java/com/user/service/UserService/services/impl/UserServiceImpl.java
+++ UserService/UserService/src/main/java/com/user/service/UserService/services/impl/UserServiceImpl.java
@@ -1,20 +1,34 @@
 package com.user.service.UserService.services.impl;
 
 import com.user.service.UserService.Exception.ResourceNotFoundException;
+import com.user.service.UserService.entities.Rating;
 import com.user.service.UserService.entities.User;
 import com.user.service.UserService.repositories.UserRepository;
 import com.user.service.UserService.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Supplier;
+
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    //communication with other service
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger= (Logger) LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public User saveUser(User user) {
@@ -30,6 +44,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getuser(String userid) {
-        return userRepository.findById(userid).orElseThrow(()-> new ResourceNotFoundException("User with given id is not found on server:"+ userid));
+        User user= userRepository.findById(userid).orElseThrow(()-> new ResourceNotFoundException("User with given id is not found on server:"+ userid));
+        //fetch rating from user service.
+       ArrayList<Rating> forobject = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
+       logger.info( "{}",forobject);
+       user.setRatingList(forobject);
+       return user;
     }
 }
